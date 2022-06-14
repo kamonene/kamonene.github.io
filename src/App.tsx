843d48167f342f700324ce37f877ec4db5be0cd4
Quack <kamoneapp@gmail.com>
2022-06-14 21:23:30 +0200

    ducks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import * as Tone from 'tone';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-ducks
-    </div>
-  );
+
+    const onClick = () => {
+        const synth = new Tone.Synth().toDestination()
+        synth.triggerAttackRelease("C4", "8n");
+
+    }
+
+    return (
+        <div className="App">
+            <button onClick={onClick}>noise!</button>
+        </div>
+    );
 }
 
 export default App;
