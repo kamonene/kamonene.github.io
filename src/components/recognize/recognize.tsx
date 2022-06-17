1d5b851ab9724936bcaa075e795ea68c179bea72
Quack <kamoneapp@gmail.com>
2022-06-17 22:51:22 +0200

    ducks

--- src/components/recognize/recognize.tsx
+++ src/components/recognize/recognize.tsx
@@ -1,18 +1,18 @@
 import React, {FunctionComponent} from 'react'
-import {pitches, pitchIncrease} from "../../utils/utils";
+
 import * as Tone from "tone";
 
 export const Recognize: FunctionComponent = () => {
 
     const onClick = () => {
-        playSequence('C4', pitches.PERFECT_FIFTH)
+        //   playSequence('C4', pitches.PERFECT_FIFTH)
     }
 
     const playSequence = (note: string, pitch: string) => {
         const synth = new Tone.Synth().toDestination()
         synth.triggerAttackRelease(note, "8n");
         setTimeout(() => {
-            synth.triggerAttackRelease(pitchIncrease(note, 7), "8n");
+          //  synth.triggerAttackRelease(pitchIncrease(note, 7), "8n");
         }, 500)
 
 
