f87c931f9cd5dab3e1af18b2a1dbf287a5b5089b
Quack <kamoneapp@gmail.com>
2022-06-25 14:08:21 +0200

    ducks

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,48 +1,65 @@
-import React, {FunctionComponent, useContext, useEffect, useState} from 'react'
-import style from './home.module.less';
+import React, {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import style from "./home.module.less";
 
-import {PracticeContainer} from "../practice-container/practice-container";
-import {Options} from "../functional/options/options";
-import {Mode} from "../../utils/constants";
-import {ctx} from "../../App";
-
+import { PracticeContainer } from "../practice-container/practice-container";
+import { Options } from "../functional/options/options";
+import { Mode } from "../../utils/constants";
+import { ctx } from "../../App";
 
 export const Home: FunctionComponent = () => {
-    const {options, setOptions} = useContext(ctx);
-    const [showOptions, setShowOptions] = useState(false)
-    useEffect(() => {
-        const keyup = (event: KeyboardEvent) => {
-            if (event.key === '4' && !event.repeat) {
-                const nextMode = options.mode === Mode.PRODUCE ? Mode.RECOGNIZE : Mode.PRODUCE
-                setOptions({...options, mode: nextMode})
-            }
-        }
-        document.addEventListener("keyup", keyup)
-        return () => {
-            document.removeEventListener("keyup", keyup)
-        }
-    }, [options])
+  const { options, setOptions } = useContext(ctx);
+  const [showOptions, setShowOptions] = useState(false);
+  useEffect(() => {
+    const keyup = (event: KeyboardEvent) => {
+      if (event.key === "4" && !event.repeat) {
+        const nextMode =
+          options.mode === Mode.PRODUCE ? Mode.RECOGNIZE : Mode.PRODUCE;
+        setOptions({ ...options, mode: nextMode });
+      }
+    };
+    document.addEventListener("keyup", keyup);
+    return () => {
+      document.removeEventListener("keyup", keyup);
+    };
+  }, [options, setOptions]);
 
-    return <div className={style.home}>
-        <div className={style.container}>
-            <button className={style.button} onClick={() => {
-                setShowOptions(!showOptions)
-            }}>
-                ≡
-            </button>
-            <h3 className={style.title}>Relative pitch</h3>
-            <p className={style.infoText}>{options.mode === Mode.RECOGNIZE ? 'Identify the interval' : 'Produce the given interval from the base note'}</p>
+  return (
+    <div className={style.home}>
+      <div className={style.container}>
+        <button
+          className={style.button}
+          onClick={() => {
+            setShowOptions(!showOptions);
+          }}
+        >
+          ≡
+        </button>
+        <h3 className={style.title}>Relative pitch</h3>
+        <p className={style.infoText}>
+          {options.mode === Mode.RECOGNIZE
+            ? "Identify the interval"
+            : "Produce the given interval from the base note"}
+        </p>
 
-            <button onClick={() => {
-                const nextMode = options.mode === Mode.PRODUCE ? Mode.RECOGNIZE : Mode.PRODUCE
-                setOptions({...options, mode: nextMode})
-            }}>
-                Change practice mode (4)
-            </button>
-            <div className={style.divider}/>
+        <button
+          onClick={() => {
+            const nextMode =
+              options.mode === Mode.PRODUCE ? Mode.RECOGNIZE : Mode.PRODUCE;
+            setOptions({ ...options, mode: nextMode });
+          }}
+        >
+          Change practice mode (4)
+        </button>
+        <div className={style.divider} />
 
-            {showOptions && <Options/>}
-            <PracticeContainer mode={options.mode}/>
-        </div>
+        {showOptions && <Options />}
+        <PracticeContainer mode={options.mode} />
+      </div>
     </div>
-}
+  );
+};
