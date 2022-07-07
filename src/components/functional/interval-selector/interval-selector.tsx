a5da8bb2d19f2235b61b680468c80ad853e4b447
Quack <kamoneapp@gmail.com>
2022-07-07 15:10:47 +0200

    ducks

--- src/components/functional/interval-selector/interval-selector.tsx
+++ src/components/functional/interval-selector/interval-selector.tsx
@@ -7,12 +7,16 @@ import { noteToNumber, numberToNote } from "../../../utils/utils";
 import * as Tone from "tone";
 import { useSynth } from "../../hooks/use-synth";
 import { useIntervalKeyBindings } from "../../hooks/use-interval-key-bindings";
+import { isMobile } from "react-device-detect";
 
 export const IntervalSelector: FunctionComponent = () => {
   useIntervalKeyBindings();
   const { options, setOptions, currentIntervalMetaData } = useContext(ctx);
   const activeIntervals = options.activeIntervals;
   useEffect(() => {
+    if (isMobile) {
+      return;
+    }
     const baseNoteSynth = new Tone.Synth().toDestination();
     const keyup = (event: KeyboardEvent) => {
       if (event.key === "1" && !event.repeat) {
@@ -21,6 +25,9 @@ export const IntervalSelector: FunctionComponent = () => {
     };
     document.addEventListener("keyup", keyup);
     return () => {
+      if (isMobile) {
+        return;
+      }
       document.removeEventListener("keyup", keyup);
 
       baseNoteSynth.triggerRelease();
